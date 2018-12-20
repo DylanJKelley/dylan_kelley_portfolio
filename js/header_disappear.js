0fa9d094e6a6b1e1aa054a049900b283ac887783
Dylan Kelley <dkelley989@west-mec.org>
2018-12-19 22:12:08 -0700

    Added Transition
    Quickly added a line to the header_disappear.js file to set a .2 second transition when hiding the h1 heading after being scrolled passed.

--- js/header_disappear.js
+++ js/header_disappear.js
@@ -4,7 +4,9 @@ window.addEventListener("scroll", function () {
     var target = document.getElementsByTagName('h1');
     if (window.pageYOffset > 100) {
         target[0].style.color = "transparent";
+        target[0].style.transition = ".2s";
     } else if (window.pageYOffset < 150) {
         target[0].style.color = "white";
+        target[0].style.transition = ".2s";
     }
 }, false);
